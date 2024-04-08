a307d0a4685e44dca132d55774fc0526c3bedc8f
kongliang1 <10223062kong_liangliang@cn.tre-inc.com>
2024-04-08 13:09:25 +0800

    sequential job example

--- redis_sequential_job_test.go
+++ redis_sequential_job_test.go
@@ -2,29 +2,38 @@ package beanq
 
 import (
 	"fmt"
+	"sync"
 	"testing"
-	"time"
-
-	"github.com/rs/xid"
 )
 
 func TestSeq(t *testing.T) {
 
-	guid := xid.NewWithTime(time.Now())
-	fmt.Printf("%+v \n", guid.String())
-	return
-
+	var wait sync.WaitGroup
 	seq := newSequential()
 
-	msg1 := NewMessage([]byte("aa"))
+	wait.Add(3)
+	// for example
+	// 3:reduce stock
+	go func() {
+		msg1 := NewMessage([]byte("aa"))
 
-	seq.In("3", *msg1)
-	msg2 := NewMessage([]byte("dd"))
-	seq.In("1", *msg2)
-
-	msg3 := NewMessage([]byte("cc"))
-	seq.In("2", *msg3)
-	seq.In("2", *msg3)
+		seq.In("3", *msg1)
+		defer wait.Done()
+	}()
+	// 1:create order
+	go func() {
+		msg2 := NewMessage([]byte("dd"))
+		seq.In("1", *msg2)
+		defer wait.Done()
+	}()
+	// 2:pay status
+	go func() {
+		msg3 := NewMessage([]byte("cc"))
+		seq.In("2", *msg3)
+		seq.In("2", *msg3)
+		defer wait.Done()
+	}()
+	wait.Wait()
 	data := seq.Sort()
 
 	fmt.Printf("%+v \n", data)
