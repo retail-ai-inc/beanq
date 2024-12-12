ef4c87bfb6a6e0d47713c83e6cb6c4700f9bfdd7
kongliang1 <10223062kong_liangliang@cn.tre-inc.com>
2024-12-12 09:11:12 +0800

    redis base method

--- helper/tool/client.go
+++ helper/tool/client.go
@@ -2,32 +2,17 @@ package tool
 
 import (
 	"context"
-	"github.com/go-redis/redis/v8"
-	"github.com/spf13/cast"
-	"sort"
-	"strings"
 )
 
 type Client struct {
-	client *redis.Client
-	prefix string
+	baseClient BaseClient
+	prefix     string
 }
 
 func (t *Client) Keys(ctx context.Context, key string) ([]string, error) {
 
-	var (
-		cursor uint64
-		keys   []string
-	)
-	for {
-		ks, ncursor := t.client.Scan(ctx, cursor, key, 50).Val()
-		keys = append(keys, ks...)
-		if ncursor <= 0 {
-			break
-		}
-		cursor = ncursor
-	}
-	return keys, nil
+	return t.baseClient.Keys(ctx, key)
+
 }
 
 func (t *Client) NodeId(ctx context.Context) string {
@@ -41,201 +26,74 @@ func (t *Client) Nodes(ctx context.Context) []Node {
 
 func (t *Client) KeySpace(ctx context.Context) ([]map[string]any, error) {
 
-	spaces, err := t.client.Info(ctx, "Keyspace").Result()
-	if err != nil {
-		return nil, err
-	}
-	m := parseInfos(spaces)
-	slic := make([]map[string]any, 0)
-	for i, v := range m {
-		nmap := make(map[string]any, 0)
-		vv := strings.Split(cast.ToString(v), ",")
-		for _, sv := range vv {
-			lv := strings.Split(sv, "=")
-			nmap[lv[0]] = lv[1]
-		}
-		nmap["dbname"] = i
-		slic = append(slic, nmap)
-	}
-	return slic, nil
+	return t.baseClient.KeySpace(ctx)
 }
 
 func (t *Client) Memory(ctx context.Context) (map[string]any, error) {
 
-	memory, err := t.client.Info(ctx, "MEMORY").Result()
-	if err != nil {
-		return nil, err
-	}
-	m := parseInfos(memory)
-	return m, nil
+	return t.baseClient.Memory(ctx)
+
 }
 
 func (t *Client) CommandStats(ctx context.Context) ([]map[string]any, error) {
 
-	command, err := t.client.Info(ctx, "Commandstats").Result()
-	if err != nil {
-		return nil, err
-	}
-
-	if strings.Contains(command, "\r\n") {
-		command = strings.ReplaceAll(command, "\r\n", "\n")
-	}
-	commands := strings.Split(command, "\n")
-
-	var commandMap Commands
-	for _, m := range commands[1:] {
-		if !strings.Contains(m, ":") {
-			continue
-		}
-		s := strings.Split(m, ":")
-		key := strings.ReplaceAll(s[0], "cmdstat_", "")
-		val := s[1]
-		vals := strings.Split(val, ",")
-		nmap := make(map[string]any, 0)
-		nmap["command"] = key
-		for _, v := range vals {
-			vv := strings.Split(v, "=")
-			nmap[vv[0]] = vv[1]
-		}
-		commandMap = append(commandMap, nmap)
-	}
-	sort.Sort(commandMap)
-	return commandMap, nil
+	return t.baseClient.CommandStats(ctx)
 }
 
 func (t *Client) Persistence(ctx context.Context) (map[string]any, error) {
 
-	persistence, err := t.client.Info(ctx, "Persistence").Result()
-	if err != nil {
-		return nil, err
-	}
-	return parseInfos(persistence), nil
+	return t.baseClient.Persistence(ctx)
+
 }
 
 func (t *Client) Server(ctx context.Context) (map[string]any, error) {
 
-	server, err := t.client.Info(ctx, "Server").Result()
-	if err != nil {
-		return nil, err
-	}
-	return parseInfos(server), nil
+	return t.baseClient.Server(ctx)
+
 }
 
 func (t *Client) Clients(ctx context.Context) (map[string]any, error) {
 
-	clients, err := t.client.Info(ctx, "Clients").Result()
-	if err != nil {
-		return nil, err
-	}
-	m := parseInfos(clients)
-	return m, nil
+	return t.baseClient.Clients(ctx)
+
 }
 
 func (t *Client) Stats(ctx context.Context) (map[string]any, error) {
 
-	stats, err := t.client.Info(ctx, "Stats").Result()
-	if err != nil {
-		return nil, err
-	}
-	return parseInfos(stats), nil
+	return t.baseClient.Stats(ctx)
+
 }
 
 func (t *Client) DbSize(ctx context.Context) (int64, error) {
-	return t.client.DBSize(ctx).Result()
+
+	return t.baseClient.DbSize(ctx)
+
 }
 
 func (t *Client) Info(ctx context.Context) (map[string]string, error) {
 
-	infoStr, err := t.client.Info(ctx).Result()
-	if err != nil {
-		return nil, err
-	}
-	info := make(map[string]string)
-	lines := strings.Split(infoStr, "\r\n")
-	for _, l := range lines {
-		kv := strings.Split(l, ":")
-		if len(kv) == 2 {
-			info[kv[0]] = kv[1]
-		}
-	}
-	return info, nil
+	return t.baseClient.Info(ctx)
+
 }
 
 func (t *Client) ClientList(ctx context.Context) ([]map[string]any, error) {
 
-	cmd := t.client.ClientList(ctx)
-	if err := cmd.Err(); err != nil {
-		return nil, err
-	}
-	data, err := cmd.Result()
-	if err != nil {
-		return nil, err
-	}
+	return t.baseClient.ClientList(ctx)
 
-	arr := strings.Split(data, "\n")
-	ldata := make(map[string]any, 0)
-	rdata := make([]map[string]any, 0, 10)
-	for _, v := range arr {
-		nv := strings.Split(v, " ")
-		for _, nvv := range nv {
-			vals := strings.Split(nvv, "=")
-			if vals[0] == "age" {
-				if vals[1] == "0" {
-					continue
-				}
-			}
-			if len(vals) < 2 {
-				continue
-			}
-			ldata[vals[0]] = vals[1]
-			rdata = append(rdata, ldata)
-		}
-	}
-	return rdata, nil
 }
 
 func (t *Client) Object(ctx context.Context, queueName string) (objstr *ObjectStruct, err error) {
 
-	obj := t.client.DebugObject(ctx, queueName)
+	return t.baseClient.Object(ctx, queueName)
 
-	str, err := obj.Result()
-	if err != nil {
-		return nil, err
-	}
-	// Value at:0x7fc38fe77cc0 refcount:1 encoding:stream serializedlength:12 lru:7878503 lru_seconds_idle:3
-	valueAt := "Value at"
-	if strings.HasPrefix(str, valueAt) {
-		str = strings.ReplaceAll(str, valueAt, "value_at")
-	}
-
-	strs := strings.Split(str, " ")
-
-	for _, s := range strs {
-		sarr := strings.Split(s, ":")
-		if len(sarr) >= 2 {
-			switch sarr[0] {
-			case "value_at":
-				objstr.ValueAt = sarr[1]
-			case "refcount":
-				objstr.RefCount = cast.ToInt(sarr[1])
-			case "encoding":
-				objstr.Encoding = sarr[1]
-			case "serializedlength":
-				objstr.SerizlizedLength = cast.ToInt(sarr[1])
-			case "lru":
-				objstr.Lru = cast.ToInt(sarr[1])
-			case "lru_seconds_idle":
-				objstr.LruSecondsIdle = cast.ToInt(sarr[1])
-			}
-		}
-	}
-	return
 }
 
 func (t *Client) ZCard(ctx context.Context, key string) (int64, error) {
-	return t.client.ZCard(ctx, key).Val(), nil
+
+	return t.baseClient.ZCard(ctx, key)
+
 }
 
 func (t *Client) Monitor(ctx context.Context) (string, error) {
-	return t.client.Do(ctx, "MONITOR").String(), nil
+	return t.baseClient.Monitor(ctx)
 }
--- helper/tool/cluster_client.go
+++ helper/tool/cluster_client.go
@@ -3,8 +3,6 @@ package tool
 import (
 	"context"
 	"github.com/go-redis/redis/v8"
-	"github.com/spf13/cast"
-	"sort"
 	"strings"
 )
 
@@ -12,26 +10,14 @@ type (
 	ClusterClient struct {
 		client     *redis.ClusterClient
 		prefix     string
-		nodeClient *redis.Client
+		baseClient *BaseClient
 		nodeId     string
 	}
 )
 
 func (t *ClusterClient) Keys(ctx context.Context, match string) ([]string, error) {
 
-	var (
-		cursor uint64
-		keys   []string
-	)
-	for {
-		ks, ncursor := t.nodeClient.Scan(ctx, cursor, match, 50).Val()
-		keys = append(keys, ks...)
-		if ncursor <= 0 {
-			break
-		}
-		cursor = ncursor
-	}
-	return keys, nil
+	return t.baseClient.Keys(ctx, match)
 }
 
 func (t *ClusterClient) NodeId(ctx context.Context) string {
@@ -90,180 +76,74 @@ func (t *ClusterClient) Nodes(ctx context.Context) []Node {
 
 func (t *ClusterClient) KeySpace(ctx context.Context) ([]map[string]any, error) {
 
-	spaces := t.nodeClient.Info(ctx, "Keyspace").Val()
-
-	m := parseInfos(spaces)
-	slic := make([]map[string]any, 0)
-	for i, v := range m {
-		nmap := make(map[string]any, 0)
-		vv := strings.Split(cast.ToString(v), ",")
-		for _, sv := range vv {
-			lv := strings.Split(sv, "=")
-			nmap[lv[0]] = lv[1]
-		}
-		nmap["dbname"] = i
-		slic = append(slic, nmap)
-	}
-	return slic, nil
+	return t.baseClient.KeySpace(ctx)
 
 }
 
 func (t *ClusterClient) Memory(ctx context.Context) (map[string]any, error) {
 
-	val := t.nodeClient.Info(ctx, "MEMORY").Val()
-	return parseInfos(val), nil
+	return t.baseClient.Memory(ctx)
 
 }
 
 func (t *ClusterClient) CommandStats(ctx context.Context) ([]map[string]any, error) {
 
-	command := t.nodeClient.Info(ctx, "Commandstats").Val()
-	if strings.Contains(command, "\r\n") {
-		command = strings.ReplaceAll(command, "\r\n", "\n")
-	}
-	commands := strings.Split(command, "\n")
-
-	var commandMap Commands
-	for _, m := range commands[1:] {
-		if !strings.Contains(m, ":") {
-			continue
-		}
-		s := strings.Split(m, ":")
-		key := strings.ReplaceAll(s[0], "cmdstat_", "")
-		val := s[1]
-		vals := strings.Split(val, ",")
-		nmap := make(map[string]any, 0)
-		nmap["command"] = key
-		for _, v := range vals {
-			vv := strings.Split(v, "=")
-			nmap[vv[0]] = vv[1]
-		}
-		commandMap = append(commandMap, nmap)
-	}
-	sort.Sort(commandMap)
-	return commandMap, nil
-
+	return t.baseClient.CommandStats(ctx)
 }
 
 func (t *ClusterClient) Persistence(ctx context.Context) (map[string]any, error) {
 
-	persistence := t.nodeClient.Info(ctx, "Persistence").Val()
-	return parseInfos(persistence), nil
+	return t.baseClient.Persistence(ctx)
 
 }
 
 func (t *ClusterClient) Server(ctx context.Context) (map[string]any, error) {
 
-	server := t.nodeClient.Info(ctx, "Server").Val()
-	return parseInfos(server), nil
+	return t.baseClient.Server(ctx)
 
 }
 
 func (t *ClusterClient) Clients(ctx context.Context) (map[string]any, error) {
 
-	clients := t.nodeClient.Info(ctx, "Clients").Val()
-	m := parseInfos(clients)
-	return m, nil
+	return t.baseClient.Clients(ctx)
 
 }
 
 func (t *ClusterClient) Stats(ctx context.Context) (map[string]any, error) {
 
-	stats := t.nodeClient.Info(ctx, "Stats").Val()
-	return parseInfos(stats), nil
+	return t.baseClient.Stats(ctx)
 
 }
 
 func (t *ClusterClient) DbSize(ctx context.Context) (int64, error) {
 
-	return t.nodeClient.DBSize(ctx).Val(), nil
-
+	return t.baseClient.DbSize(ctx)
 }
 
 func (t *ClusterClient) Info(ctx context.Context) (map[string]string, error) {
 
-	infoStr := t.nodeClient.Info(ctx).Val()
-
-	info := make(map[string]string)
-	lines := strings.Split(infoStr, "\r\n")
-	for _, l := range lines {
-		kv := strings.Split(l, ":")
-		if len(kv) == 2 {
-			info[kv[0]] = kv[1]
-		}
-	}
-	return info, nil
+	return t.baseClient.Info(ctx)
 
 }
 
 func (t *ClusterClient) ClientList(ctx context.Context) ([]map[string]any, error) {
 
-	data := t.nodeClient.ClientList(ctx).Val()
-
-	arr := strings.Split(data, "\n")
-	ldata := make(map[string]any, 0)
-	rdata := make([]map[string]any, 0, 10)
-	for _, v := range arr {
-		nv := strings.Split(v, " ")
-		for _, nvv := range nv {
-			vals := strings.Split(nvv, "=")
-			if vals[0] == "age" {
-				if vals[1] == "0" {
-					continue
-				}
-			}
-			if len(vals) < 2 {
-				continue
-			}
-			ldata[vals[0]] = vals[1]
-			rdata = append(rdata, ldata)
-		}
-	}
-	return rdata, nil
+	return t.baseClient.ClientList(ctx)
 
 }
 
 func (t *ClusterClient) Object(ctx context.Context, queueName string) (objstr *ObjectStruct, err error) {
 
-	str := t.nodeClient.DebugObject(ctx, queueName).Val()
-	// Value at:0x7fc38fe77cc0 refcount:1 encoding:stream serializedlength:12 lru:7878503 lru_seconds_idle:3
-	valueAt := "Value at"
-	if strings.HasPrefix(str, valueAt) {
-		str = strings.ReplaceAll(str, valueAt, "value_at")
-	}
+	return t.baseClient.Object(ctx, queueName)
 
-	strs := strings.Split(str, " ")
-
-	for _, s := range strs {
-		sarr := strings.Split(s, ":")
-		if len(sarr) >= 2 {
-			switch sarr[0] {
-			case "value_at":
-				objstr.ValueAt = sarr[1]
-			case "refcount":
-				objstr.RefCount = cast.ToInt(sarr[1])
-			case "encoding":
-				objstr.Encoding = sarr[1]
-			case "serializedlength":
-				objstr.SerizlizedLength = cast.ToInt(sarr[1])
-			case "lru":
-				objstr.Lru = cast.ToInt(sarr[1])
-			case "lru_seconds_idle":
-				objstr.LruSecondsIdle = cast.ToInt(sarr[1])
-			}
-		}
-	}
-	return objstr, nil
 }
 
 func (t *ClusterClient) ZCard(ctx context.Context, key string) (int64, error) {
 
-	return t.nodeClient.ZCard(ctx, key).Val(), nil
+	return t.baseClient.ZCard(ctx, key)
 
 }
 
 func (t *ClusterClient) Monitor(ctx context.Context) (string, error) {
-
-	return t.nodeClient.Do(ctx, "MONITOR").String(), nil
-
+	return t.baseClient.Monitor(ctx)
 }
