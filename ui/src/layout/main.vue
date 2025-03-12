f172f3e5cfd2c60232613b133f7ddfb285b97f55
kongliang1 <10223062kong_liangliang@cn.tre-inc.com>
2025-03-12 08:39:46 +0800

    I18n causes page initialization issues

--- ui/src/layout/main.vue
+++ ui/src/layout/main.vue
@@ -1,12 +1,8 @@
 <template>
   <router-view v-slot="{Component}">
     <component :is="Component"/>
-    <En/>
-    <Ja/>
   </router-view>
 </template>
 
 <script setup>
-import En from "./i18n/en.vue";
-import Ja from "./i18n/ja.vue";
 </script>
