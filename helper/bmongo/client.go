39f3efd18e7378d108e8659c8dd59d152ab7f5ba
kongliang1 <10223062kong_liangliang@cn.tre-inc.com>
2026-01-28 16:53:20 +0800

    use fmt.Errorf

--- helper/bmongo/client.go
+++ helper/bmongo/client.go
@@ -710,7 +710,7 @@ func (t *BMongo) MongoDetail(ctx context.Context) ([]CollectionState, error) {
 			//{"scale",1024*1024}, this value is not working
 		}
 		if cmderr := t.database.RunCommand(ctx, cmd).Decode(&colState); err != nil {
-			err = errors.Join(err, errors.New(fmt.Sprintf("Error getting stats for %s: %+v", collection, cmderr)))
+			err = errors.Join(err, fmt.Errorf("error getting stats for %s: %+v", collection, cmderr))
 			continue
 		}
 		colState.Name = collection
